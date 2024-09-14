5f1b1bfa3adc94f57149e6670353bca86bc0aa19
jailcomfranssa <jail_son_0@hotmail.com>
2024-09-13 23:21:33 -0300

    first commit

---
+++ src/test/java/jailcomfranssa/com/github/PetShopping/PetShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package jailcomfranssa.com.github.PetShopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
